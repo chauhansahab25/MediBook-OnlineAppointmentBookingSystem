03424f78967031dbb84ec392b33299e7bed26bf2
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-04-22 11:42:46 +0530

    Cleared main branch

--- AuthService/DTOs/UpdateProfileDto.cs
+++
@@ -1,8 +0,0 @@
- namespace AuthService.DTOs;
-
-public class UpdateProfileDto
-{
-    public string? FullName { get; set; }
-    public string? Phone { get; set; }
-    public string? ProfilePicUrl { get; set; }
-}
