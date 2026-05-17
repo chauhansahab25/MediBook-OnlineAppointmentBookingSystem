356e1fc8839086b3eac297964a8c25e389bafc2c
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-05-17 22:27:03 +0530

    Fix Folder Structure

--- ProviderService/DTOs/SyncProviderDto.cs
+++
@@ -1,9 +0,0 @@
-namespace ProviderService.DTOs;
-
-public class SyncProviderDto
-{
-    public int UserId { get; set; }
-    public string FullName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string? Phone { get; set; }
-}
